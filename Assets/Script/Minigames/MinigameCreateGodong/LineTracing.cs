3cc7001601567b47c65c03e888e1daf68848543d
christianseiji.cs@gmail.com <Ch28871c33@#>
2022-11-15 00:24:46 +0700

    Completed Circle Renderer

---
+++ Assets/Script/Minigames/MinigameCreateGodong/LineTracing.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MasakGodong;
+
+public class LineTracing : MonoBehaviour
+{
+    NGonRenderer lines;
+    int lineCount;
+    float lineWidth;
+
+    Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+       lines = GetComponent<NGonRenderer>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HandleTouchBegin(Touch touch) 
+    {
+        Vector2 touchWorldSpace = cam.ScreenToWorldPoint(touch.position); 
+        Debug.Log(lines.PointInLines(touchWorldSpace));
+    }
+
+    public void HandleTouchMoved(Touch touch) {
+        
+    }
+
+    public void HandleTouchStopped(Touch touch) {
+        
+    }
+}
